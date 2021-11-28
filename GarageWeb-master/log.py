110632607b1d89cde45eb0c2a9209f39dafdccfd
enrrique98 <enrriquedr@gmail.com>
2021-11-28 17:30:46 -0600

    prueba

--- GarageWeb-master/log.py
+++ GarageWeb-master/log.py
@@ -36,9 +36,9 @@ try:
           logfile.write(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- Door Opening/Closing \n"))
           logfile.close()
 	        print(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- Door Opening/Closing \n"))
-           while GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
+          while GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
              time.sleep(.5)
-           else:
+            else:
              if GPIO.input(16) == GPIO.LOW:  #Door is Closed
                   logfile = open("/home/pi/GarageWeb/static/log.txt","a")
                   logfile.write(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- Door Closed \n"))
