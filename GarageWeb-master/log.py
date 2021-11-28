5b88f7a67fd860982e8160a7cc00ad3e1e6b8f5a
enrrique98 <enrriquedr@gmail.com>
2021-11-28 17:36:32 -0600

    prueba

--- GarageWeb-master/log.py
+++ GarageWeb-master/log.py
@@ -9,7 +9,7 @@ logfile.write(datetime.now().strftime("     Program Starting -- %Y/%m/%d -- %H:%
 logfile.close()
 print(datetime.now().strftime("     Program Starting -- %Y/%m/%d -- %H:%M  -- Hello! \n"))
 
-print ("Control + C to exit Program")
+print " Control + C to exit Program"
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -24,21 +24,21 @@ DoorOpenTimerMessageSent = 1  #Turn off messages until timer is started
 
 try:
         while 1 >= 0:
-         time.sleep(1) #Door Open Timer has Started
-         if DoorOpenTimer==1: 
-           currentTimeDate=datetime.strptime(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),'%Y-%m-%d %H:%M:%S')
-           if (currentTimeDate-TimeDoorOpened).seconds>900 and DoorOpenTimerMessageSent==0:
-            print("Your Garage Door has been Open for 15 minutes")
-            DoorOpenTimerMessageSent = 1
+         time.sleep(1)
+         if DoorOpenTimer == 1:  #Door Open Timer has Started
+	         currentTimeDate = datetime.strptime(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),'%Y-%m-%d %H:%M:%S')
+                 if (currentTimeDate - TimeDoorOpened).seconds > 900 and DoorOpenTimerMessageSent == 0:
+                    print "Your Garage Door has been Open for 15 minutes"
+                    DoorOpenTimerMessageSent = 1
 
          if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:  #Door Status is Unknown
-          logfile = open("/home/pi/GarageWeb/static/log.txt","a")
-          logfile.write(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- Door Opening/Closing \n"))
-          logfile.close()
-	        print(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- Door Opening/Closing \n"))
-          while GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
+           logfile = open("/home/pi/GarageWeb/static/log.txt","a")
+           logfile.write(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- Door Opening/Closing \n"))
+           logfile.close()
+	   print(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- Door Opening/Closing \n"))
+           while GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
              time.sleep(.5)
-            else:
+           else:
              if GPIO.input(16) == GPIO.LOW:  #Door is Closed
                   logfile = open("/home/pi/GarageWeb/static/log.txt","a")
                   logfile.write(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- Door Closed \n"))
