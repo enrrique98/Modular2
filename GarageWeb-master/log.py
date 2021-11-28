7f3d205fa12ed1c09b02c6cc67d3e56ad205b576
enrrique98 <enrriquedr@gmail.com>
2021-11-28 17:22:36 -0600

    2.0

--- GarageWeb-master/log.py
+++ GarageWeb-master/log.py
@@ -27,9 +27,9 @@ try:
          time.sleep(1) #Door Open Timer has Started
          if DoorOpenTimer==1: 
            currentTimeDate=datetime.strptime(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'),'%Y-%m-%d %H:%M:%S')
-           if(currentTimeDate-TimeDoorOpened).seconds>900 and DoorOpenTimerMessageSent==0:
-                    print "Your Garage Door has been Open for 15 minutes"
-                    DoorOpenTimerMessageSent = 1
+           if (currentTimeDate-TimeDoorOpened).seconds>900 and DoorOpenTimerMessageSent==0:
+            print("Your Garage Door has been Open for 15 minutes")
+            DoorOpenTimerMessageSent = 1
 
          if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:  #Door Status is Unknown
            logfile = open("/home/pi/GarageWeb/static/log.txt","a")
